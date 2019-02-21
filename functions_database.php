932140bd023abd9fa4247cae3bcbd41ca6735cb4
yuuske.miyazaki <y38y38@gmail.com>
2019-02-21 23:02:10 +0900

    debug

--- functions_database.php
+++ functions_database.php
@@ -6,6 +6,22 @@
    		$rows = $wpdb->get_row("SELECT * FROM wp_products WHERE id = $id"); 	
         return $rows;
     }
+    function GetColumns()
+    {
+		global $wpdb;
+
+   		$columns = $wpdb->get_results("SHOW COLUMNS FROM  wp_products"); 	
+        $columns_num = count($columns);
+        var_dump($columns_num);
+
+        $columns_name = array();
+        for($i = 0;$i < $columns_num;$i++) {
+            $columns_name[$i] =  $columns[$i]->Field;
+        }
+  
+        return $columns_name;
+    }
+
 
 ?>
 
--- input_spacs.php
+++ input_spacs.php
@@ -1,10 +1,89 @@
 <?php
-    function InputSpacs (){
-                $result  = "<form method= \" post \" >";
-                $result .= "<input type = \" text \" name = \" i_name \">";
-                $result .= "<input type = \" submit \" value= \" send \">";
+/*
+    function MakeSmartphoneTable() {
+            $table = array();
+            $table = {
+                    "i_name", 
+                    "i_price", 
+                    "i_height", 
+                    "i_width", 
+                    "i_thickness", 
+                    "i_weight_gram", 
+                    "i_water_proof", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price", 
+                    "i_price" 
+            };
+        
+    }
+*/
+/*
+    function MakeInputForm($input_spac)
+    {
+         $result  = "<form method= \" post \" >";
+         $result .= "<input type = \" text \" name = \" i_name \"><br>";
+         $result .= "name    <input type = \" text \" name = \" i_name \"><br>";
+         $result .= "price   <input type = \" text \" name = \" i_price \"><br>";
+         $result .= "height  <input type = \" text \" name = \" i_height \"><br>";
+         $result .= "width   <input type = \" text \" name = \" i_width \"><br>";
 
-                $result .= "</form>";
+         $result .= "<input type = \" submit \" value= \" send \"><br>";
+
+         $result .= "</form>";
+         return $result;
+
+    }
+*/
+    function InputSpacs (){
+        $result = GetColumns();
+        var_dump($result);
+        //$result = MakeInputForm()
+        $result = "hello";
         return  $result;
     }
     add_Shortcode('InputSpacsCode', 'InputSpacs');
