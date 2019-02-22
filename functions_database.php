14aeeed7f2d522a063ef8a271116896d97d78f8d
yuuske.miyazaki <y38y38@gmail.com>
2019-02-23 00:28:34 +0900

    debug

--- functions_database.php
+++ functions_database.php
@@ -1,18 +1,18 @@
 <?php
     function GetDataBaseFormId($id)
     {
-		global $wpdb;
+        global $wpdb;
 
-   		$rows = $wpdb->get_row("SELECT * FROM wp_products WHERE id = $id"); 	
+        $rows = $wpdb->get_row("SELECT * FROM wp_products WHERE id = $id"); 	
         return $rows;
     }
     function GetColumns()
     {
-		global $wpdb;
+        global $wpdb;
 
-   		$columns = $wpdb->get_results("SHOW COLUMNS FROM  wp_products"); 	
+        $columns = $wpdb->get_results("SHOW COLUMNS FROM  wp_products"); 	
         $columns_num = count($columns);
-        var_dump($columns_num);
+        //var_dump($columns_num);
 
         $columns_name = array();
         for($i = 0;$i < $columns_num;$i++) {
--- input_spacs.php
+++ input_spacs.php
@@ -1,89 +1,55 @@
 <?php
-/*
-    function MakeSmartphoneTable() {
-            $table = array();
-            $table = {
-                    "i_name", 
-                    "i_price", 
-                    "i_height", 
-                    "i_width", 
-                    "i_thickness", 
-                    "i_weight_gram", 
-                    "i_water_proof", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price", 
-                    "i_price" 
-            };
-        
-    }
-*/
-/*
-    function MakeInputForm($input_spac)
+
+    function MakeInputForm($spac_list)
     {
-         $result  = "<form method= \" post \" >";
-         $result .= "<input type = \" text \" name = \" i_name \"><br>";
-         $result .= "name    <input type = \" text \" name = \" i_name \"><br>";
-         $result .= "price   <input type = \" text \" name = \" i_price \"><br>";
-         $result .= "height  <input type = \" text \" name = \" i_height \"><br>";
-         $result .= "width   <input type = \" text \" name = \" i_width \"><br>";
+        $result  = "<form method= \"post\" >";
+        $result  .= "<table>";
+        $list_num = count($spac_list);
+        for ($i = 0; $i < $list_num ;$i ++) {
+            $result  .= "<tr>";
+            $result  .= "<td>";
+            $result  .= $spac_list[$i];
+            //var_dump($spac_list[$i]);
+            $result  .= "</td>";
+            $result  .= "<td>";
+            $spac_value_name = "i_" . $spac_list[$i];
+            //var_dump($spac_value_name);
+            $result  .= "<input type = \"text\" name = \"$spac_value_name\">";
+            //$result  .= "$spac_value_name";
+            $result  .= "</td>";
+            $result  .= "</tr>";
 
-         $result .= "<input type = \" submit \" value= \" send \"><br>";
+        }
+        $result  .= "</table>";
+        $result .= "<button type =\"submit\"  name = \"send \">send</button>";
+        
+        $result .= "</form>";
+        /*
+        $result = "<form method= \"post\">";
+        $result .= "<input type = \"text\" name = \"aa\" >";
+        $result .= "<input type = \"submit\" value= \"send\">";
 
-         $result .= "</form>";
-         return $result;
+        $result .="</form>";
+         */
+        return $result;
 
     }
-*/
+    function InputSpacePost() {
+        //var_dump($_POST);
+    }
+    function InputSpaceGet() {
+        $colums_list = GetColumns();
+        $result = MakeInputForm($colums_list );
+        return $result;
+    }
+
     function InputSpacs (){
-        $result = GetColumns();
-        var_dump($result);
-        //$result = MakeInputForm()
-        $result = "hello";
+        var_dump($_SERVER["REQUEST_METHOD"]);
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $result = InputSpacePost();
+        } else {
+            $result = InputSpaceGet();
+        }
         return  $result;
     }
     add_Shortcode('InputSpacsCode', 'InputSpacs');
