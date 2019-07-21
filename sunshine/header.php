b053c87315ceba8d9370f23a7085cf87e3833db6
Yuusuke <y38y38@gmail.com>
2019-07-21 23:39:33 +0900

    debug

--- sunshine/header.php
+++ sunshine/header.php
@@ -5,6 +5,7 @@
         <title><?php wp_title( ' | ', true, 'right' ); ?></title>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
         <?php wp_head(); ?>
+
 <!-- add contents start-->
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-59997071-3"></script>
@@ -24,15 +25,24 @@
           enable_page_level_ads: true
      });
 </script>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css" type="text/css" />	
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" />	
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style_spac.css" type="text/css" />	
 <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 
 <!-- add contents end-->
+
+
+
     </head>
-    <body <?php body_class(); ?>>
+
+
+
+    <!-- <body <?php body_class(); ?> link="#3333FF" vlink="#FFFFFF"> -->
+    <body <?php body_class(); ?> >
 
         <header class="page-header">
-            <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="Sunshine Blog" width="227" height="33"></a></h1>
+            <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Update Gadget</a></h1>
         </header>
 
 
